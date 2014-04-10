f936289074df21997e9079205a1b05634ebc6858
Cristian Rinaldi <csrinaldi@gmail.com>
2014-04-10 10:52:29 -0300

    Incorporacion de polymer, vinculacion con Angular, mail.html

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,53 +1,65 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Bootstraping para nodeJS
+ * 
+ * TODO estructurar aplicacion en el servidor
+ * 
+ * @author Cristian Rinaldi <csrinaldi@gmail.com>
  */
 var express = require("express"),
-    app     = express(),
-    http    = require("http"),
-    server  = http.createServer(app);
+        app = express(),
+        http = require("http"),
+        fs = require('fs');
 
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
-app.all('*', function(req, res, next){
-  if (!req.get('Origin')) return next();
-  // use "*" here to accept any origin
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, POST');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  // res.set('Access-Control-Allow-Max-Age', 3600);
-  if ('OPTIONS' == req.method) return res.send(200);
-  next();
-});
+app.use(express.compress());
 
-app.get('/', function(req, res) {
-  res.json(200, {view: "Home", result:"Not Found"});
+console.log(__dirname);
+
+app.use(express.static(__dirname + '/public'));
+
+server = http.createServer(app);
+
+app.all('*', function(req, res, next) {
+    if (!req.get('Origin'))
+        return next();
+    // use "*" here to accept any origin
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    // res.set('Access-Control-Allow-Max-Age', 3600);
+    if ('OPTIONS' == req.method)
+        return res.send(200);
+    next();
 });
 
 app.post('/users/login', function(req, res) {
-  res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 app.post('/users/:id/edit', function(req, res) {
-  res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 app.get('/users/:id', function(req, res) {
-  res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 app.get('/tutorials', function(req, res) {
-  res.send("Hello world!");
+    res.send("Hello world!");
 });
 
 server.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+    console.log("Node server running on http://localhost:3000");
+    console.log("The index.html is ready");
 });
+
+
+
 
 
 
