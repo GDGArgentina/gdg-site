a7683b33ae6d43a99aff4d9a207fa521deac6a16
csrinaldi <csrinaldi@gmail.com>
2014-05-19 16:04:34 -0300

    Uso de ngAnimated de Angular para la transicion de pantallas con ui-routing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var app = angular.module('GdgApp',
-        ['ui.router', 'GdgApp.Controllers', 'GdgApp.Services']
-        );
+        ['ui.router', 'ngAnimate', 'GdgApp.Controllers', 'GdgApp.Services']
+);
 
 app.config(['$injector', '$stateProvider', '$urlRouterProvider', function($injector, $stateProvider, $urlRouterProvider) {
         //
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise("/home");
-
         //
         // Now set up the states
         $stateProvider
@@ -18,43 +17,28 @@ app.config(['$injector', '$stateProvider', '$urlRouterProvider', function($injec
                     onEnter: function($stateParams, $state) {
                     },
                     controller: function($scope) {
-                        //PageTransitions.init();
-                        //PageTransitions.nextPage(17);
                     }
                 })
-                /*.state('state1.list', {
-                 url: "/list",
-                 templateUrl: "partials/state1.list.html",
-                 controller: function($scope) {
-                 $scope.items = ["A"., "List", "Of", "Items"];
-                 }
-                 })*/
                 .state('objetive', {
                     url: "/objetive",
                     templateUrl: "views/objetive.html"
                 })
-        /*.state('state2.list', {
-         url: "/list",
-         templateUrl: "partials/state2.list.html",
-         controller: function($scope) {
-         $scope.things = ["A", "Set", "Of", "Things"];
-         }
-         })*/
+
     }]);
 
-app.run(['Transition', '$rootScope', '$urlRouter', '$state', function(transition, $rootScope, $urlRouter, $state) {
-        transition.init(angular.element("#pt-main"), 28);
+/*app.run(['Transition', '$rootScope', '$urlRouter', '$state', function(transition, $rootScope, $urlRouter, $state) {
+        //transition.init(angular.element("#pt-main"), 28);
 
         $rootScope.$on('$stateChangeStart',
                 function(event, toState, toParams, fromState, fromParams) {
-                    transition.leave();
+                    //transition.leave();
                 });
 
         $rootScope.$on('$stateChangeSuccess',
                 function(e) {
-                    transition.reveal();
+                    //transition.reveal();
                 });
-    }]);
+    }]);*/
 
 app.onSuccess = function(response) {
     console.log("onSucess");
